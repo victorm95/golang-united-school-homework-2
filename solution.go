aa08c96eec99c58f829802f2dd9425adce985541
victorm95 <victormiguel.riveramorales.1995@gmail.com>
2022-02-21 19:37:23 -0500

    fix: change contant names

--- solution.go
+++ solution.go
@@ -5,21 +5,21 @@ import "math"
 type Sides int
 
 const (
-	Circle   Sides = 0
-	Triangle Sides = 3
-	Square   Sides = 4
+	SideCircle   Sides = 0
+	SideTriangle Sides = 3
+	SideSquare   Sides = 4
 )
 
 func CalcSquare(sideLen float64, sidesNum Sides) float64 {
 	switch sidesNum {
-	case Circle:
+	case SideCircle:
 		return math.Pi * math.Pow(sideLen, 2)
 
-	case Triangle:
+	case SideTriangle:
 		h := math.Sqrt(math.Pow(sideLen, 2) - math.Pow(sideLen/2, 2))
 		return h * sideLen / 2
 
-	case Square:
+	case SideSquare:
 		return math.Pow(sideLen, 2)
 
 	default:
