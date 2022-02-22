176ba9c22d9a05c0a751313e0a77be36c87a083d
victorm95 <victormiguel.riveramorales.1995@gmail.com>
2022-02-21 19:17:47 -0500

    format: fix indentation

--- solution.go
+++ solution.go
@@ -5,24 +5,24 @@ import "math"
 type Sides int
 
 const (
-		Circle Sides = 0
-		Triangle Sides = 3
-		Square Sides = 4
+	Circle   Sides = 0
+	Triangle Sides = 3
+	Square   Sides = 4
 )
 
 func CalcSquare(sideLen float64, sidesNum Sides) float64 {
-		switch sidesNum {
-		case Circle:
-				return math.Pi * sideLen
+	switch sidesNum {
+	case Circle:
+		return math.Pi * sideLen
 
-		case Triangle:
-				h := math.Sqrt(math.Pow(sideLen/2, 2) - math.Pow(sideLen, 2))
-				return h * sideLen / 2
+	case Triangle:
+		h := math.Sqrt(math.Pow(sideLen/2, 2) - math.Pow(sideLen, 2))
+		return h * sideLen / 2
 
-		case Square:
-				return math.Pow(sideLen, 2)
+	case Square:
+		return math.Pow(sideLen, 2)
 
-		default:
-				return 0
-		}
+	default:
+		return 0
+	}
 }
